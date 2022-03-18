620d86a098390a9c7fc78b6e2253ba438b0305f4
dianelabz <dbarboza@4geeks.co>
2022-03-18 22:29:37 +0000

    ya se matematicas casi terminado

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const fromDollarToYen = function(dollar) {
-    let euro = dollar / 1.2;
-    let yen = euro * 127.9;
-    return yen;
-}
-const fromEuroToDollar = function(euro){
-    let dollar = euro * 1.2;
-    return dollar;
+let oneEuroIs = {
+    "JPY": 127.9, // japan yen
+    "USD": 1.2, // us dollar
+    "GBP": 0.8, // british pound
 }
 console.log("Hello World");
 
@@ -15,4 +11,18 @@ function sum(a, b) {
 console.log(fromEuroToDollar(3.5))
 console.log(sum(7, 3))
 
-module.exports = { sum, fromDollarToYen };
+function fromDollarToYen(dollar) {
+    let euro = (dollar / 1.2);
+    let yen = (euro * 127.9);
+    return yen;
+}
+function fromEuroToDollar(euro) {
+    return euro * oneEuroIs.USD
+}
+function fromYenToPound(Yen) {
+    let euro = (Yen / 127.9);
+    let pound = (euro * 0.8);
+    return pound;
+}
+
+module.exports = { sum, fromDollarToYen, fromEuroToDollar, fromYenToPound };
